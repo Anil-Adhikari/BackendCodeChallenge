12ede734ab67f5580908b61fbc320b061373a15c
anil-adhikari <link.anil200@gmail.com>
2018-10-18 22:15:29 -0500

    project structure refactored

--- src/test/java/com/anil/codechallenge/ApplicationDetailDbAccessTest.java
+++ src/test/java/com/anil/codechallenge/ApplicationDetailDbAccessTest.java
@@ -1,4 +1,4 @@
-package com.anil.codechallenge.backendcodechallenge;
+package com.anil.codechallenge;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
